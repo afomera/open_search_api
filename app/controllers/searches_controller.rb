7772e97d8489bfda95af728bf54efd3aea8ee42d
Andrew Fomera <andrew@zerlex.net>
2018-06-28 23:14:50 -0500

    Make previous searches unique and display activity
    We want to filter out the non-unique searches
    and display previous times searched in the results.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,14 +1,16 @@
 class SearchesController < ApplicationController
   def index
-    @previous_searches = Search.all
+    @previous_searches = Search.select(:query).distinct
+    @search_activity = Search.where(query: params[:search])
     if params[:search].present?
       @results = NasaImages.new(query_params).search
     end
   end
 
   def create
-    @previous_searches = Search.all
-    Search.create(query: params[:search])
+    @previous_searches = Search.select(:query).distinct
+    @search_activity = Search.where(query: params[:search])
+    Search.create(query: params[:search]) unless params[:search].blank?
     @results = NasaImages.new(query_params).search
     render :index
   end
